b0c67c437e3bfa17fa612ef9c6f5c50a6e72628f
Di <azazarova99@mail.ru>
2020-04-14 00:22:53 +0300

    feat: add html- and file-loader, html-plugins

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,64 @@
+const path = require('path') //подтягивает path из devDependencies
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') //образение к package.json
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = { //изменив эти константы тут, они автоматичесски меняются и в дев, и в билд
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/' 
+
+}
+
+module.exports = {
+
+    externals: { //обращение к дев и билд
+        paths: PATHS //доступ к константе из других конфигов, если этого не сделать, придется константы копировать в dev/build
+    },
+    //точка входа, путь к входному файлу js
+    entry: {
+        app: PATHS.src //стандарт, в src лежит весь исходный код
+        //можно не указывать index.js, вебпак сам поймет 
+        //можно писать через PATHS
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].js`, //[] на случай нескольких entry, текущий name берется из ярлыка в entry
+        path: PATHS.dist, //webpack  всегда ищет точку выхода. начиная с корня проекта, 
+        //чтобы искал корреткно, указывается path
+        publicPath: '/'
+    },
+    module: {
+        rules:[{ //array with objects
+            test: /\.js$/, //parameters
+            loader:  'babel-loader', //all .js files handle with this loader
+            exclude: '/node_modules/' //for speeding compilation, exclude several files(many libraries are already processed with babel)
+        }, { //array with objects
+            test: /\.(png|jpg|gif|svg)$/, 
+            loader:  'file-loader', //all .img files handle with this loader
+            options: {
+                name: '[name].[ext]'
+            }
+        }, {
+            test: /\.css$/, 
+            use: [
+                MiniCssExtractPlugin.loader, 
+                'css-loader'
+            ], //помогает разделить css и js
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: `${PATHS.assets}css/[name].css` //ссылается на ярлык app сверху, т.е. на выходе будем иметь app.css
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATHS.src}/index.html`,
+            filename: './index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: `${PATHS.src}/img`, to:  `${PATHS.assets}img` }
+        ])
+    ],
+}
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -5,7 +5,8 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const devWebpackConfig = merge(baseWebpackConfig, {
     mode: 'development',
     devServer: {
-        port: 8085, //по дефолту 8080, так как иногда работаем со вторым сервером, слушает сервер 8080, если он занят - будет ошибка
+        contentBase:  baseWebpackConfig.externals.paths.dist, //отвечает за то, где будет открываться вебпак
+        port: 8084, //по дефолту 8080, так как иногда работаем со вторым сервером, слушает сервер 8080, если он занят - будет ошибка
         overlay: { //shows errors not in console, but in browser screen, black background, red letters on error place
             warnings: true,
             errors: true
